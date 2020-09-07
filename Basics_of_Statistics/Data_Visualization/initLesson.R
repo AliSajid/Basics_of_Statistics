9ad8a28c49177cea0a32f9342c804de9bd1104e3
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-09-06 21:07:14 -0400

    Updated the basics of Ddata visualization lesson

--- Basics_of_Statistics/Data_Visualization/initLesson.R
+++ Basics_of_Statistics/Data_Visualization/initLesson.R
@@ -1,3 +1,3 @@
-assign("cars", openintro::cars, envir=globalenv())
-assign("mpg.midsize", cars[cars$type=="midsize","mpgCity"], envir=globalenv())
+assign("cars", cars93, envir=globalenv())
+assign("mpg.midsize", cars[cars$type=="midsize","mpg_city"], envir=globalenv())
 swirl_options(swirl_logging = TRUE)
